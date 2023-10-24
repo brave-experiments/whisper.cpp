63d2f2c7c06e95d4162ca5612a46180f85df46b8
boocmp <boocmp@yandex.ru>
2023-10-24 20:18:16 +0700

    CUDA link.

--- bindings/go/whisper.go
+++ bindings/go/whisper.go
@@ -9,7 +9,7 @@ import (
 // CGO
 
 /*
-#cgo LDFLAGS: -lwhisper -lm -lstdc++ -lcudart
+#cgo LDFLAGS: -lwhisper -lm -lstdc++ -lcudart -L/usr/local/cuda/lib64 -L/opt/cuda/lib64
 #cgo darwin LDFLAGS: -framework Accelerate
 #include <whisper.h>
 #include <stdlib.h>
